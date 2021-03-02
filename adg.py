5f07e4837c99997515b2a8828ad9c717c57aa72d
Mannymunjuri <emmanuel.munjuri@gmail.com>
2021-03-02 17:39:15 +0300

    initial commits

---
+++ adg.py
@@ -0,0 +1,22 @@
+from flask import Flask,redirect,url_for
+app = Flask(__name__)
+
+@app.route('/admin')
+def hello_admin():
+    return "Hello admin"
+
+@app.route('/guest/<userTyp>')
+def hello_guest(userTyp):
+    return "Hello guest"
+
+@app.route('/user/<typ>')
+def hello_user(typ):
+    if typ == "admin":
+        return redirect(url_for("hello_admin"))
+    else:
+        return redirect(url_for("hello_guest",userTyp=typ))
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
+
